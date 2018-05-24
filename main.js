866de929b9dd7c3d1e08b9632bab2b7aa28d6110
Daniel Cabaceira <daniel.cabaceira@gmail.com>
2018-05-24 15:19:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,195 +1,55 @@
-var Sequelize =
-    require('sequelize');
-
-
-
-var
-    mongo_uri = "",
-    mysql_uri =
-    "";
-
-
-
-if (process.env.NODE_ENV ===
-    "production") {
-
-    // mongo_uri = 'mongodb://admin:MVJVTVJRQVCKVDCP@sl-eu-lon-2-portal.9.dblayer.com:26110,sl-eu-lon-2-portal.10.dblayer.com:26110/compose?authSource=admin&ssl=true';
-
-    // mysql_uri = 'mysql://admin:ZJBQRDMEHMADJHUQ@sl-eu-lon-2-portal.11.dblayer.com:26392/compose';
-
-    mongo_uri
-        =
-        'mongodb://localhost:27017/test';
-
-    mysql_uri
-        =
-        'mysql://api:123qwe@localhost:3306/test';
-
-} else {
-
-    mongo_uri
-        =
-        'mongodb://localhost:27017/test';
-
-    mysql_uri
-        =
-        'mysql://api:123qwe@localhost:3306/test';
-
-}
-
-
-
-const
-    operatorsAliases = {
-
-        $eq: Sequelize.Op.eq,
-
-        $ne: Sequelize.Op.ne,
-
-        $gte: Sequelize.Op.gte,
-
-        $gt: Sequelize.Op.gt,
-
-        $lte: Sequelize.Op.lte,
-
-        $lt: Sequelize.Op.lt,
-
-        $not: Sequelize.Op.not,
-
-        $in: Sequelize.Op.in,
-
-        $notIn: Sequelize.Op.notIn,
-
-        $is: Sequelize.Op.is,
-
-        $like: Sequelize.Op.like,
-
-        $notLike: Sequelize.Op.notLike,
-
-        $iLike: Sequelize.Op.iLike,
-
-        $notILike: Sequelize.Op.notILike,
-
-        $regexp: Sequelize.Op.regexp,
-
-        $notRegexp: Sequelize.Op.notRegexp,
-
-        $iRegexp: Sequelize.Op.iRegexp,
-
-        $notIRegexp: Sequelize.Op.notIRegexp,
-
-        $between: Sequelize.Op.between,
-
-        $notBetween: Sequelize.Op.notBetween,
-
-        $overlap: Sequelize.Op.overlap,
-
-        $contains: Sequelize.Op.contains,
-
-        $contained: Sequelize.Op.contained,
-
-        $adjacent: Sequelize.Op.adjacent,
-
-        $strictLeft: Sequelize.Op.strictLeft,
-
-        $strictRight: Sequelize.Op.strictRight,
-
-        $noExtendRight: Sequelize.Op.noExtendRight,
-
-        $noExtendLeft: Sequelize.Op.noExtendLeft,
-
-        $and: Sequelize.Op.and,
-
-        $or: Sequelize.Op.or,
-
-        $any: Sequelize.Op.any,
-
-        $all: Sequelize.Op.all,
-
-        $values: Sequelize.Op.values,
-
-        $col: Sequelize.Op.col
-
-    };
-
-
-
-// Create a new conntection to MongoDB server
-
-mongoose.connect(mongo_uri);
-
-// Create a new connection to MySQL server
-
-var
-    sequelize = new
-Sequelize(mysql_uri, {
-    operatorsAliases: operatorsAliases,
-    logging: false
-});
-
-
-
-const
-    db = {
-
-        'Boardmodel': require('./mysql/board_model')(sequelize,
-            Sequelize),
-
-        'Board': require('./mysql/board')(sequelize,
-            Sequelize),
-
-        'Patient': require('./mysql/patient')(sequelize,
-            Sequelize),
-
-        'Sensormodel': require('./mysql/sensor_model')(sequelize,
-            Sequelize),
-
-        'Sensor': require('./mysql/sensor')(sequelize,
-            Sequelize),
-
-        'UserVitabox': require('./mysql/user_vitabox')(sequelize,
-            Sequelize),
-
-        'User': require('./mysql/user')(sequelize,
-            Sequelize),
-
-        'Vitabox': require('./mysql/vitabox')(sequelize,
-            Sequelize),
-
-
-
-        'RecordCheck': require('./mongodb/record_check'),
-
-        'RecordTemp': require('./mongodb/record_temp'),
-
-        'RecordOld': require('./mongodb/record_old'),
-
-        'Log': require('./mongodb/log'),
-
-        'Error': require('./mongodb/error'),
-
-        'Warning': require('./mongodb/warning')
-
+require('./models/db').sequelize.sync().then(
+    () => {
+        const express = require('express')
+        const app = express()
+        const bodyParser = require('body-parser');
+
+        const user = require('./controllers/user');
+        const server = require("http").createServer();
+
+
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        app.use(bodyParser.json());
+
+        app.use((req, res, next) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Accept-Version");
+            res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+            res.header('Access-Control-Allow-Credentials', true);
+
+            console.log("\x1b[36m" + req.method, req.url + "\x1b[0m");
+
+            if (req.method === "OPTIONS") {
+                return res.send(200);
+            }
+
+            if (req.headers.authorization) {
+                user.validateToken(req.headers.authorization).then(
+                    client => {
+                        if (client) req.client = client;
+                        else req.client = null;
+                        next();
+                    },
+                    error => {
+                        req.client = null;
+                        next();
+                    });
+            } else {
+                req.client = null;
+                next();
+            }
+        });
+
+        app.use('/', express.static('public'));
+        require("./router")(app);
+
+        server.on("request", app);
+        server.listen(3000, () => console.log('Example app listening on port 3000!'))
+    },
+    error => {
+        console.log("connection refused", error);
+        process.exit(1);
     }
-
-
-
-Object.keys(db).forEach(modelName => {
-
-    if (db[modelName].associate)
-        db[modelName].associate(db);
-
-});
-
-
-
-db.sequelize =
-    sequelize;
-
-db.mongoose =
-    mongoose.connection;
-
-
-
-module.exports =
-    db;
+)
