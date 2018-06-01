f79fc64f6cad10aca7eda9f778801cc42871b46c
danielcabaceira <daniel.cabaceira@gmail.com>
2018-06-01 02:49:04 +0100

    Controllers adicionados

---
+++ controllers/technical-form/categorias.js
@@ -0,0 +1,21 @@
+var db = require('../models/db')
+
+exports.adicionarCat = (req, res) => {
+    db.categorias.create({
+        Super_Categoria: req.body.Super_Categoria,
+        Nome: req.body.Nome
+    });
+}
+
+exports.updateCat = (req, res, next) => {
+    db.categorias.update({
+        Super_Categoria: req.body.Super_Categoria,
+        Nome: req.body.Nome
+    }, {
+        where: req.params.CatId
+    }).then(
+        (rowsUpdated) => {
+            res.json(rowsUpdated)
+        }
+    ).catch(next)
+}
---
+++ controllers/technical-form/descricao_tecnica.js
@@ -0,0 +1,23 @@
+var db = require('../models/db')
+
+exports.adicionarDescTec = (req, res) => {
+    db.descricao_tecnica.create({
+        Estrutura: req.body.Estrutura,
+        Superficie: req.body.Superficie,
+        Elementos_Acessorios: req.body.Elementos_Acessorios
+    });
+}
+
+exports.updateDescTec = (req, res, next) => {
+    db.descricao_tecnica.update({
+        Estrutura: req.body.Estrutura,
+        Superficie: req.body.Superficie,
+        Elementos_Acessorios: req.body.Elementos_Acessorios
+    }, {
+        where: req.params.DescTecId
+    }).then(
+        (rowsUpdated) => {
+            res.json(rowsUpdated)
+        }
+    ).catch(next)
+}
