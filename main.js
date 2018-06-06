fe91af9c70d1eae53683e7b4a20f79bf4e5a4072
carlossilva2 <cmiguelrsilva@gmail.com>
2018-06-06 16:55:13 +0100

    Auto create roles and admin account

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-var Server = require('./server');
+var Server = require("./server");
+var db = require("./models/db");
 
-require('./models/db').sequelize.sync().then(
+require("./models/db").sequelize.sync().then(
   () => {
-    var s = new Server(process.env.PORT || 3000);
-    s.start();
+    var s = new Server();
+    s.start(process.env.PORT || 3000);
+    require("./controllers/role").register();
+    require("./controllers/user").createAdmin();
   },
   error => {
     console.log("connection refused", error);
     process.exit(1);
   }
-)
+);
